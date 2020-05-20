959c66e6fed0fc8cc8d0703ca6df32d313c400ff
Clara Vetter <clarasophie.v@gmail.com>
2020-05-20 22:03:50 +0200

    created a package apis
    - added the weather api
    - created a package apis
    - different modules for unit tests

--- apis/test_dive_api.py
+++ apis/test_dive_api.py
@@ -1,74 +1,12 @@
-""" diveAPI.py
-Module to retrieve data from the divesites API
+# -*- coding: utf-8 -*-
+""" 
+test_dive_api.py
+@author: Clara
+Module to unit test dive_api.py
 """
 
 import unittest
-import requests
-import json
-
-class DiveSite:
-    """ Properties of a dive site """
-    def __str__(self):
-        """ Convert object to string """
-        return "Class DiveSite:" + '\n' + \
-            'ident = ' + str(self.ident) + '\n' + \
-            'lat = ' + str(self.lat) + '\n' + \
-            'lng = ' + str(self.lng) + '\n' + \
-            'name = ' + str(self.name) + '\n' + \
-            'currents =  ' + str(self.currents) + '\n' + \
-            'description =  ' + str(self.description) + '\n' + \
-            'distance =  ' + str(self.distance) + '\n' + \
-            'equipment =  ' + str(self.equipment) + '\n' + \
-            'hazards =  ' + str(self.hazards) + '\n' + \
-            'marinelife =  ' + str(self.marinelife) + '\n' + \
-            'maxdepth =  ' + str(self.maxdepth) + '\n' + \
-            'mindepth =  ' + str(self.mindepth) + '\n' + \
-            'predive =  ' + str(self.predive) + '\n' + \
-            'water =  ' + str(self.water) + '\n'
-        
-    ident = None
-    lat = None
-    lng = None
-    name = None
-    currents = None
-    description = None
-    distance = None
-    equipment = None
-    hazards = None
-    marinelife = None
-    maxdepth = None
-    mindepth = None
-    predive = None
-    water = None
-
-def dive_sites(lat, lng, dist):
-    """ Request dive sites around lat and long within radius dist (nautical miles)"""
-    # Request data 
-    _response = requests.get("http://api.divesites.com/?mode=sites&lat="+str(lat)+"&lng="+str(lng)+"&dist="+str(dist))
-    
-    # Create result
-    _diveSites = []
-    if _response.status_code == 200:
-        _json_response = _response.json()          
-        # Parse sites
-        for site in  _json_response['sites']:
-            _diveSite = DiveSite()
-            _diveSite.ident = site['id']
-            _diveSite.lat = site['lat']
-            _diveSite.lng = site['lng']
-            _diveSite.name = site['name']
-            _diveSite.currents = site['currents']
-            _diveSite.description = site['description']
-            _diveSite.equipment = site['equipment']
-            _diveSite.hazards = site['hazards']
-            _diveSite.marinelife = site['marinelife']
-            _diveSite.maxdepth = site['maxdepth']
-            _diveSite.mindepth = site['mindepth']
-            _diveSite.predive = site['predive']
-            _diveSites.append(_diveSite)
-        
-    return _diveSites
-
+import dive_api
 
 class TestDiveAPI(unittest.TestCase):
     """ Unit test cases"""
@@ -77,7 +15,7 @@ class TestDiveAPI(unittest.TestCase):
         _lat = 47.6031537682643
         _lng = -122.336164712906
         _dist = 20
-        _sites = dive_sites(_lat, _lng, _dist) 
+        _sites = dive_api.get_dive_sites(_lat, _lng, _dist) 
         self.assertEqual(len(_sites), 67, "Wrong dive site count")
 
     def test_location1_cnt2(self):
@@ -85,7 +23,7 @@ class TestDiveAPI(unittest.TestCase):
         _lat = 47.6031537682643
         _lng = -122.336164712906
         _dist = 1
-        _sites = dive_sites(_lat, _lng, _dist) 
+        _sites = dive_api.get_dive_sites(_lat, _lng, _dist) 
         self.assertEqual(len(_sites), 1, "Wrong dive site count")
 
     def test_location1_attributes(self):
@@ -93,7 +31,7 @@ class TestDiveAPI(unittest.TestCase):
         _lat = 47.6031537682643
         _lng = -122.336164712906
         _dist = 20
-        _sites = dive_sites(_lat, _lng, _dist) 
+        _sites = dive_api.get_dive_sites(_lat, _lng, _dist) 
         for _site in _sites:
             self.assertNotEqual(_site.ident, '', "Empty ident in "+str(_site))
             self.assertNotEqual(_site.lat, '', "Empty lat in "+str(_site))
@@ -105,7 +43,7 @@ class TestDiveAPI(unittest.TestCase):
         _lat = 47.6031537682643
         _lng = -122.336164712906
         _dist = 20
-        _sites = dive_sites(_lat, _lng, _dist) 
+        _sites = dive_api.get_dive_sites(_lat, _lng, _dist) 
         _found = False
         self.assertNotEqual(len(_sites), 0, "Wrong dive site count")        
         for _site in _sites:
@@ -118,7 +56,7 @@ class TestDiveAPI(unittest.TestCase):
         _lat = -8.350785
         _lng = 116.038628
         _dist = 200
-        _sites = dive_sites(_lat, _lng, _dist) 
+        _sites = dive_api.get_dive_sites(_lat, _lng, _dist) 
         self.assertEqual(len(_sites), 12, "Wrong dive site count")
 
     def test_location2_cnt2(self):
@@ -126,7 +64,7 @@ class TestDiveAPI(unittest.TestCase):
         _lat = -8.350785
         _lng = 116.038628
         _dist = 1
-        _sites = dive_sites(_lat, _lng, _dist) 
+        _sites = dive_api.get_dive_sites(_lat, _lng, _dist) 
         self.assertEqual(len(_sites), 1, "Wrong dive site count")
 
     def test_location2_attributes(self):
@@ -134,7 +72,7 @@ class TestDiveAPI(unittest.TestCase):
         _lat = -8.350785
         _lng = 116.038628
         _dist = 1
-        _sites = dive_sites(_lat, _lng, _dist) 
+        _sites = dive_api.get_dive_sites(_lat, _lng, _dist) 
         for _site in _sites:
             self.assertNotEqual(_site.ident, '', "Empty ident in "+str(_site))
             self.assertNotEqual(_site.lat, '', "Empty lat in "+str(_site))
@@ -147,7 +85,7 @@ class TestDiveAPI(unittest.TestCase):
         _lat = -8.350785
         _lng = 116.038628
         _dist = 20
-        _sites = dive_sites(_lat, _lng, _dist) 
+        _sites = dive_api.get_dive_sites(_lat, _lng, _dist) 
         _found = False
         self.assertNotEqual(len(_sites), 0, "Wrong dive site count")        
         for _site in _sites:
@@ -158,5 +96,3 @@ class TestDiveAPI(unittest.TestCase):
 # Execute main
 if __name__ == '__main__':
     unittest.main()
-
-
